a8fc3c5d9f193b9484f56aa4be7bd7b15bdb23c4
Justin Parisio <114594154+jparisio@users.noreply.github.com>
2025-03-13 20:21:50 -0400

    gallery added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useRef } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -5,6 +6,7 @@ import {
   useLocation,
 } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import Lenis from "lenis";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
@@ -12,8 +14,19 @@ import Gallery from "./pages/Gallery/Gallery";
 import Navbar from "./components/Navbar";
 
 import "./App.css";
+import "lenis/dist/lenis.css";
 
 export default function App() {
+  useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis({
+      autoRaf: true,
+    });
+    lenis.on("scroll", (e) => {
+      console.log(e);
+    });
+  }, []);
+
   return (
     <Router>
       <Navbar />
--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,5 +1,52 @@
 import Curve from "../../transitions/Curve";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import "./Gallery.css";
 
 export default function Gallery() {
-  return <Curve></Curve>;
+  const images = [
+    "./gallery.jpg",
+    "./contact.jpg",
+    "./home.jpg",
+    "./about.jpg",
+    "./services.jpg",
+    "./gallery.jpg",
+    "./contact.jpg",
+    "./home.jpg",
+  ];
+
+  // Track the window scroll progress (0 to 1)
+  const { scrollYProgress } = useScroll();
+
+  const x = useTransform(scrollYProgress, [0, 1], ["35%", "-35%"]);
+  const imgX = useTransform(scrollYProgress, [0, 1], [-200, 0]);
+
+  return (
+    <Curve>
+      <motion.div className="gallery-container">
+        <motion.div className="scroll-container" style={{ x }}>
+          {images.map((item, index) => (
+            <Image key={index} index={index} src={item} imgX={imgX} />
+          ))}
+        </motion.div>
+      </motion.div>
+    </Curve>
+  );
 }
+
+const Image = ({ src, alt = "gallery-img", index, imgX }) => {
+  return (
+    <motion.div
+      className="gallery-image-container"
+      initial={{ y: 400 }}
+      animate={{ y: 0 }}
+      transition={{
+        duration: 0.9,
+        ease: [0.76, 0, 0.24, 1],
+        delay: 0.015 * index + 0.2,
+      }}
+    >
+      <motion.img src={src} alt={alt} style={{ x: imgX }} />
+    </motion.div>
+  );
+};
